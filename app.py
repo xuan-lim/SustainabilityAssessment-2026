c0ff029e5ad1926044edc333e013f3d0bb8e7a1e
xuan-lim <xuan.lim@ecloudvalley.com>
2026-01-13 12:23:49 +0800

    delete the "["

--- app.py
+++ app.py
@@ -884,11 +884,11 @@ elif st.session_state.step == 3: self.render_materiality()
 elif st.session_state.step == 4: self.render_tcfd()
 elif st.session_state.step == 5: self.render_hrdd()
 elif st.session_state.step == 6: self.render_finish()
-]
             
 if __name__ == "__main__":
 app = SustainabilityAssessment()
 app.run()
+
 
 
 
